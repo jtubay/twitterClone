eb3c8ea90797696f867be7698bd4e944b056a02b
Jean Tubay <jean.tubay.92@gmail.com>
2020-07-06 20:32:06 -0400

    added validation

--- server/server.js
+++ server/server.js
@@ -13,8 +13,26 @@ app.get('/', (req, res) => {
     })
 })
 
+isValidMew = (mew) => {
+    return mew.name && mew.name.toString().trim() !== '' && 
+    mew.content && mew.content.toString().trim() !== '';
+}
+
 app.post('/mews', (req, res) => {
+    const {name, content} = req.body
     console.log(req.body)
+    if(isValidMew(req.body)){
+        const me = {
+            name: name.toString(),
+            content: content.toString()
+        }
+
+    }else {
+        res.status(422);
+        res.json({
+            message: "hey, must have content"
+        })
+    }
 })
 
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`))
